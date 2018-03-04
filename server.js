5e94e3d741a45947262be929eb871864e110e01e
Ben Keating <ben.l.keating@gmail.com>
2018-03-04 16:18:32 +0000

    =third attempt at project page publish

--- server.js
+++ server.js
@@ -56,6 +56,11 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
+});
 app.get('/bad', (req, res) => {
   res.send({
     errorCode: 001,
